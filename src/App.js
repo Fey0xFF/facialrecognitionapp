b3131afb5b16f4cf01984a999e80f7448f53db87
Anthony Bendas <anthony.bendas@gmail.com>
2019-02-28 08:20:00 -0800

    navigation component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>test</p>
+        <Navigation />
+        {
+        	/*<Logo />
+        <ImageLinkForm />
+        <FaceRecognition />*/
+    }
       </div>
     );
   }
