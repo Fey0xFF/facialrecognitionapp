1574b0766e3638ccee49dd10eb0b393bfcc5141e
Anthony Bendas <anthony.bendas@gmail.com>
2019-03-04 22:31:15 -0800

    user element

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,6 +5,7 @@ const Navigation = () => {
 		<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
 			<p className='f3 link dim black underline pa3 pointer'> Sign Out </p>
 			<p className='f3 link dim black underline pa3 pointer'> Sign In </p>
+			<p> current user </p>
 		</nav>
 	);
 }
