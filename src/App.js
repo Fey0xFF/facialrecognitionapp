dbfbfcd9bf99e7ee53a3c4f75faf8f9e528dca4d
Anthony Bendas <anthony.bendas@gmail.com>
2019-03-10 10:42:00 -0700

    clarifai API connected

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Navigation from './components/Navigation/Navigation';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+ apiKey: 'cf22cc04efd74db09226f4cb0378be40'
+});
 
 const particlesOptions = {
   "particles": {
@@ -118,6 +123,29 @@ const particlesOptions = {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log("click");
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+      function(response) {
+        console.log(response);
+      },
+      function(err) {
+        // there was an error
+      }
+    );
+  }
+
   render() {
     return (
       <div className="App">
@@ -128,10 +156,12 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit}
+        />
         {
-        	/*<Logo />
-
+        	/*
         <FaceRecognition />*/
     }
       </div>
