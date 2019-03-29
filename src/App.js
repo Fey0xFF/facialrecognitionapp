f163611263400cb41e28d0c4a39381597264cac0
Anthony Bendas <anthony.bendas@gmail.com>
2019-03-28 20:38:33 -0700

    register component posts

--- src/App.js
+++ src/App.js
@@ -133,11 +133,25 @@ class App extends Component {
       imageURL:"",
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        entries: 0,
+        joined: ''
+      }
     }
   }
 
-  
+  loadUser = (data) => {
+    this.setState({user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+    }})
+  }
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -205,7 +219,7 @@ class App extends Component {
           : (
             route === 'signin'
             ? <Signin onRouteChange={this.onRouteChange}/>
-            : <Register onRouteChange={this.onRouteChange}/>
+            : <Register onRouteChange={this.onRouteChange} loadUser={this.loadUser}/>
           ) 
         }
       </div>
