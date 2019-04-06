1c758de90d9dc4369038d153c42dc4f4fb8ef7c0
Anthony Bendas <anthony.bendas@gmail.com>
2019-04-05 19:49:20 -0700

    title change, url text, build ready

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
 	return (
 		<div>
 			<p className="f3">
-				{'Enter path to face image!'}
+				{'Enter URL of face image for magical detection!'}
 			</p>
 			<div className="center">
 				<div className="form center pa4 br3 shadow-5">
